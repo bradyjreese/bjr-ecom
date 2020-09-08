7457567fd87d37b66d87d41ce74fcb5ff56f47f0
northerreese <bradyj.reese@gmail.com>
2020-09-08 10:41:39 -0600

    developed navbar and header actions for setting new links

--- src/components/header-navbar/navbar.js
+++ src/components/header-navbar/navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
+import { setNavbarLinks } from '../../actions';
 
 class Navbar extends Component {
     render() {
