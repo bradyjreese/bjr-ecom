5adeacf65cc4c9adf64acc4fd47233459c83b590
northerreese <bradyj.reese@gmail.com>
2020-09-08 14:53:26 -0600

    designed a bit of the purchase history

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,14 +8,92 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase 1',
-                amount: 8.02
+                total: 8.02,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
             },
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 18.40
-            }
+                total: 19.40,
+                orderNumber: 'B81237896',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Test 2',
+                    shippingAddress: '422 West Lehi Ave'
+                }
+            },
+            {
+                _id: 2,
+                total: 11.57,
+                orderNumber: 'C872398466',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Test 3',
+                    shippingAddress: '8988 East Blvd'
+                }
+            },
+            {
+                _id: 3,
+                total: 12.99,
+                orderNumber: 'D9088273874',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Test 4',
+                    shippingAddress: '7777 Spartan Drive'
+                }
+            },
+            {
+                _id: 4,
+                total: 7.65,
+                orderNumber: 'E837ysdg721',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Test 5',
+                    shippingAddress: '3333 Anywhere Lane'
+                }
+            },
+            {
+                _id: 5,
+                total: 1.99,
+                orderNumber: 'F90923jkl',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Test 6',
+                    shippingAddress: '3321 Plase please Me'
+                }
+            },
+            {
+                _id: 6,
+                total: 27.99,
+                orderNumber: 'G90823489G',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Test 7',
+                    shippingAddress: '098789 LIHSHDFJKH Dr'
+                }
+            },
+            {
+                _id: 7,
+                total: 5.55,
+                orderNumber: 'H1249324789sdf',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Test 8',
+                    shippingAddress: '543 Why Me Lane'
+                }
+            },
         ]
     })
 }
