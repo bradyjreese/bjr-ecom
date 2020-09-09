9af5863f9f13dc85f047a12f750db493e09c62ee
northerreese <bradyj.reese@gmail.com>
2020-09-09 10:29:29 -0600

    added password button to account form

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { reduxForm, Field } from 'redux-form';
 
-import { FormInput, FormButton } from '../formFields';
+import { FormInput, FormButton, LongGrayButton } from '../formFields';
 
 import history from '../../history'
 
@@ -66,23 +66,15 @@ class AccountInformationForm extends Component {
                     component={FormInput}
                 />
 
-                {/* <Field
-                    className='account-information-form__password'
-                    type='password'
-                    title='Password'
-                    placeholder='Password'
-                    name='password'
-                    component={FormInput}
-                />
-
                 <Field
-                    className='account-information-form__login'
-                    onClick={() => history.push('/account')}
-                    type='submit'
-                    title='Login'
-                    name='login'
-                    component={FormButton}
-                /> */}
+                    className='account-information-form__change-password'
+                    onClick={() => console.log('show passwords')}
+                    type='button'
+                    labelTitle='Password'
+                    title='Change Password'
+                    name='change-password'
+                    component={LongGrayButton}
+                />
             </form>
         )
     }
