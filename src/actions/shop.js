10ee4524c6e6407c657e356de06c774a4cc61e09
northerreese <bradyj.reese@gmail.com>
2020-09-10 11:44:53 -0600

    designed shop stuff

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -63,56 +63,56 @@ export function fetchShopProducts() {
             {
                 _id: 0,
                 title: 'JavaScript in the Browser',
-                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center. This means a high-fidelity gaming experience enclosed in a quiet and bold design, with the ability to discover thousands of games across four generations, all with more playing and less waiting.',
+                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center.',
                 price: 1.99,
                 belongsTo: [0, 1]
             },
             {
                 _id: 1,
                 title: 'Graph Database',
-                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center. This means a high-fidelity gaming experience enclosed in a quiet and bold design, with the ability to discover thousands of games across four generations, all with more playing and less waiting.',
+                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center.',
                 price: 1.99,
                 belongsTo: [0, 5]
             },
             {
                 _id: 2,
                 title: 'Full Stack Development',
-                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center. This means a high-fidelity gaming experience enclosed in a quiet and bold design, with the ability to discover thousands of games across four generations, all with more playing and less waiting.',
+                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center.',
                 price: 1.99,
                 belongsTo: [0, 1, 4]
             },
             {
                 _id: 3,
                 title: 'Full Stack Development',
-                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center. This means a high-fidelity gaming experience enclosed in a quiet and bold design, with the ability to discover thousands of games across four generations, all with more playing and less waiting.',
+                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center.',
                 price: 1.99,
                 belongsTo: [0, 1, 6]
             },
             {
                 _id: 4,
                 title: 'JavaScript Development',
-                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center. This means a high-fidelity gaming experience enclosed in a quiet and bold design, with the ability to discover thousands of games across four generations, all with more playing and less waiting.',
+                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center.',
                 price: 1.99,
                 belongsTo: [0, 1]
             },
             {
                 _id: 5,
                 title: 'User Interface Design',
-                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center. This means a high-fidelity gaming experience enclosed in a quiet and bold design, with the ability to discover thousands of games across four generations, all with more playing and less waiting.',
+                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center.',
                 price: 1.99,
                 belongsTo: [0, 2]
             },
             {
                 _id: 6,
                 title: 'User Experience Design',
-                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center. This means a high-fidelity gaming experience enclosed in a quiet and bold design, with the ability to discover thousands of games across four generations, all with more playing and less waiting.',
+                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center.',
                 price: 1.99,
                 belongsTo: [0, 2]
             },
             {
                 _id: 7,
-                title: 'Advanced Object Oriented Programming',
-                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center. This means a high-fidelity gaming experience enclosed in a quiet and bold design, with the ability to discover thousands of games across four generations, all with more playing and less waiting.',
+                title: 'Advanced OOP',
+                description: 'Xbox Series X is our fastest, most powerful console ever, designed for a console generation that has you at its center.',
                 price: 1.99,
                 belongsTo: [0, 6]
             }
