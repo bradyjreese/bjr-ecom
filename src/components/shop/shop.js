c630252195ded9e7adb49d10171f7b7f4389faad
northerreese <bradyj.reese@gmail.com>
2020-09-10 14:11:48 -0600

    started cart componenet

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,8 +4,16 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
 
 class Shop extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            showCart: true
+        }
+    }
 
     componentDidMount() {
         const headerLinks = [
@@ -33,6 +41,8 @@ class Shop extends Component {
     }
 
     render() {
+        return <ShopCart classname='shop__cart' />
+
         return(
             <div className='shop'>
                 <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar' />
@@ -45,6 +55,11 @@ class Shop extends Component {
                         })
                     }
                 </div>
+
+                {
+                    this.state.showCart ? <ShopCart className='shop_cart' /> : ''
+                }
+
                 {/* shop cart button */}
             </div>
         )
