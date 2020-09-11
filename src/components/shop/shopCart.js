e851578918d69c980f1f7a20c8199f621cbf13ab
northerreese <bradyj.reese@gmail.com>
2020-09-11 10:55:21 -0600

    new order route

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import history from '../../history';
 
 import CartProduct from './cartProduct';
 import CartButton from './cartButton';
@@ -27,7 +28,7 @@ function CartFooter({ className, products }) {
     const price = 7.96;
     return (
         <div className={`${className} cart-footer`}>
-            <a className='cart-footer__checkout'>
+            <a onClick={() => history.push('/order/review')} className='cart-footer__checkout'>
                 Checkout
             </a>
 
