4542730d1cb917bed24c793e30ee5b23b72996ec
northerreese <bradyj.reese@gmail.com>
2020-09-11 09:29:52 -0600

    added cart styles

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -5,6 +5,7 @@ import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
 import ShopCart from './shopCart';
+import CartButton from './cartButton';
 
 class Shop extends Component {
     constructor() {
@@ -40,6 +41,14 @@ class Shop extends Component {
         this.props.filterProductsWithQuery(fields);
     }
 
+    handleAddToCart = () => {
+        if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
+            document.getElementById('shop-cart').classList.remove('cart-hidden');
+        } else {
+            document.getElementById('shop-cart').classList.add('cart-hidden');
+        }
+    }
+
     render() {
         // return <ShopCart classname='shop__cart' />
 
@@ -56,11 +65,13 @@ class Shop extends Component {
                     }
                 </div>
 
-                {
-                    this.state.showCart ? <ShopCart className='shop_cart' /> : ''
-                }
+                {this.state.showCart ? <ShopCart className='shop__cart' /> : ''}
 
-                {/* shop cart button */}
+                <CartButton
+                    onClick={this.handleAddToCart}
+                    className='shop__cart-button'
+                    icon='fas fa-cart-plus'
+                />
             </div>
         )
     }
